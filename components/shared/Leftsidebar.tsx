99b05820c8c7427286cd372bd86932e6bbbc8888
Harish3005-cloud <harish.e.naidu@gmail.com>
2025-09-27 21:43:11 +0530

    Main_code

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -1,8 +1,27 @@
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
+
 function Leftsidebar() {
     return (
-        <div>
-            <h1>Leftsidebar</h1>
-        </div>
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+               {sidebarLinks.map((link) => (
+                <Link
+                href={link.route}
+                key={link.label}
+                className="leftsidebar_link"
+                
+                >
+                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                <p className="text-light-1 max-lg:hidden">{link.label}</p>
+                </Link>
+               ))}
+            </div>
+
+        </section>
     );
 }
 
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,13 +1,42 @@
+import {SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
+
 import Link from "next/link";
 import Image from "next/image";
+
 
 function Topbar() {
     return (
         <nav className="topbar">
             <Link href="/" className="flex items-center gap-4">
-                <Image src="/logo.svg" alt="logo" width={28} height={28} />
+                <Image src="/assets/logo.svg" alt="logo" width={108} height={108}  />
                 <p className="text-heading3-bold text-light-1 max-xs:hidden">CampusConnect</p>
             </Link>
+
+
+
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                <SignedIn>
+                    <SignOutButton>
+                    <div className="flex cursor-pointer">
+                        <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+
+                        
+                    </div>
+                    </SignOutButton>
+                </SignedIn>
+                <OrganizationSwitcher
+                appearance={{
+                    elements: {
+                    organizationSwitcher: "py-5 px-2" 
+                    }
+                }}
+                />
+
+                </div>
+                
+            </div>
+
         </nav>
     );
 }
